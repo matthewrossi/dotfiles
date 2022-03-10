bfb412e89547f47131b1ad624efa83b9f2540804
Matthew Rossi <mattw.rossi@gmail.com>
2022-03-10 11:55:44 +0100

    fish: fix fzf key-bindings

--- fish/.config/fish/conf.d/03_fzf.fish
+++ fish/.config/fish/conf.d/03_fzf.fish
@@ -1,26 +1,26 @@
 # Fuzzy finder
 
-# TODO: investigate why key-bindings are not working with fish
+if command -sq fzf
+    # Use fd when it is installed
+    if command -sq fd
+        set -gx FZF_DEFAULT_COMMAND 'fd'
+        set -gx FZF_CTRL_T_COMMAND 'fd'
+        set -gx FZF_ALT_C_COMMAND 'fd -t d'
+    end
+    
+    # use bat and tree to show a preview of files and directories
+    # NOTE: Ubuntu/Debian install `bat` as `batcat`, to be consistent with
+    # other distributions set up a symlink by running:
+    # `ln -s /usr/bin/batcat ~/.local/bin/bat`
+    if command -sq bat; and command -sq tree
+        set -gx FZF_CTRL_T_OPTS "--select-1 --exit-0 --preview \"bash -c \'(bat --style=plain --color=always --line-range :200 {} || tree -aC {}) 2> /dev/null | head -200'\""
+        set -gx FZF_ALT_C_OPTS "--select-1 --exit-0 --preview 'tree -aC {} | head -200'"
+    end
 
-# Use fd when it is installed
-if command -sq fd
-    set -gx FZF_DEFAULT_COMMAND 'fd'
-    set -gx FZF_CTRL_T_COMMAND 'fd'
-    set -gx FZF_ALT_C_COMMAND 'fd -t d'
+    fzf_key_bindings
+    
+    # Use Nord color scheme
+    set -agx FZF_DEFAULT_OPTS '
+    --color fg:#D8DEE9,bg:#2E3440,hl:#A3BE8C,fg+:#D8DEE9,bg+:#434C5E,hl+:#A3BE8C
+    --color pointer:#BF616A,info:#4C566A,spinner:#4C566A,header:#4C566A,prompt:#81A1C1,marker:#EBCB8B'
 end
-
-# use bat and tree to show a preview of files and directories
-# NOTE: Ubuntu/Debian install `bat` as `batcat`, to be consistent with
-# other distributions set up a symlink by running:
-# `ln -s /usr/bin/batcat ~/.local/bin/bat`
-if command -sq bat; and command -sq tree
-    set -gx FZF_CTRL_T_OPTS "--select-1 --exit-0 --preview \"bash -c \'(bat --style=plain --color=always --line-range :200 {} || tree -C {}) 2> /dev/null | head -200'\""
-    set -gx FZF_ALT_C_OPTS "--select-1 --exit-0 --preview 'tree -C {} | head -200'"
-end
-
-source /usr/share/doc/fzf/examples/key-bindings.fish
-
-# Use Nord color scheme
-set -agx FZF_DEFAULT_OPTS '
---color fg:#D8DEE9,bg:#2E3440,hl:#A3BE8C,fg+:#D8DEE9,bg+:#434C5E,hl+:#A3BE8C
---color pointer:#BF616A,info:#4C566A,spinner:#4C566A,header:#4C566A,prompt:#81A1C1,marker:#EBCB8B'
